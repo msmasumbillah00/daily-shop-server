46e8a350cdc3c6772126bee17ec7950d6e983efd
Md Masum Billah <mdmasumbillahms00@gmail.com>
2025-07-30 10:56:24 +0800

    add new products successfull

--- controller/ProductController.js
+++ controller/ProductController.js
@@ -2,11 +2,34 @@ const Product = require("../modals/Product");
 
 const getAllProcucts = async (req, res, next) => {
   try {
+    const products = await Product.find();
+    res.json(products);
   } catch (error) {
     console.log(error);
   }
 };
 
+const addNewProducts = async (req, res, next) => {
+  try {
+    const { name, price, stock, category, image } = req.body;
+
+    const newProduct = new Product({
+      name,
+      price,
+      stock,
+      category,
+      image,
+    });
+
+    await newProduct.save();
+
+    res.send("Product added successfully!");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error adding product.");
+  }
+};
 module.exports = {
   getAllProcucts,
+  addNewProducts,
 };
--- server.js
+++ server.js
@@ -2,21 +2,30 @@ const express = require("express");
 const mongoose = require("mongoose");
 const connectDB = require("./db");
 const PORT = 3000;
+const bodyParser = require("body-parser");
+
+const {
+  getAllProcucts,
+  addNewProducts,
+} = require("./controller/ProductController");
 
 const app = express();
 
 app.use(express.json());
-
-connectDB("mongodb://127.0.0.1:27017/daily-shop")
-  // connectDB(
-  //   "mongodb+srv://msmasumbillah0000:mdstudentsmasumallclass@cluster0.pf0bweu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-  // )
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// connectDB("mongodb://127.0.0.1:27017/daily-shop")
+connectDB(
+  "mongodb+srv://msmasumbillah0000:mdstudentsmasumallclass@cluster0.pf0bweu.mongodb.net/daily-shop?retryWrites=true&w=majority&appName=Cluster0"
+)
   .then(() => {
     console.log("Connected to MongoDB");
     console.log("Connected to DB:", mongoose.connection.name);
   })
   .catch((err) => console.error("❌ Connection error:", err));
 
+app.get("/products", getAllProcucts);
+app.post("/products/add", addNewProducts);
 app.get("/", (req, res) => {
   res.json({ name: "MD MAsum Billah" });
 });
